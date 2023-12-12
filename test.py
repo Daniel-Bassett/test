8812e0dc1f8926ce09f1e5427b0b6c23718ed21c
Daniel B <daniel.m.bassett@outlook.com>
2023-12-12 06:19:14 -0600

    test

--- test.py
+++ test.py
@@ -61,12 +61,14 @@ def url_parse(internal_hrefs):
     return normalized_urls
 
 
+@st.cache_resource
 def get_driver():
     options = Options()
     options.add_argument('--disable-gpu')
     options.add_argument('--headless')
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    # return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    return webdriver.Chrome(options=options)
 
 
 def get_summary(page_text):
