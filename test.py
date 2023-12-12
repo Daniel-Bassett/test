04dae9060a4da01e452fd37037ba3f726511a5f0
Daniel B <daniel.m.bassett@outlook.com>
2023-12-12 06:30:55 -0600

    test

--- test.py
+++ test.py
@@ -68,8 +68,7 @@ def get_driver():
     options.add_argument('--headless')
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
     options.add_experimental_option("detach", True)
-    # return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    return webdriver.Chrome(options=options)
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 
 def get_summary(page_text):
